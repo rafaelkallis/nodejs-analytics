6217d1ececd543c24b957e03d4e733e60b6e8ab2
rafaelkallis <rk@rafaelkallis.com>
2016-12-14 21:57:35 +0100

    added outer network

--- app/initialize.js
+++ app/initialize.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  Promise.each(Array.from(document.getElementsByClassName('katex')), element => Katex.render(element.innerHTML, element));
+  renderMathInElement(document.body, { delimiters: [ {left: "$", right: "$", display: false} ] });
 });
--- brunch-config.js
+++ brunch-config.js
@@ -2,13 +2,11 @@ module.exports = {
   files: {
     javascripts: {
       joinTo: {
-        'js/vendor.js': /^(?!app)/,
         'js/app.js': /^app/
       }
     },
     stylesheets: {
       joinTo: {
-        'css/vendor.css': /^(?!app)/,
         'css/app.css': /^app/
       }
     }
@@ -16,17 +14,5 @@ module.exports = {
 
   plugins: {
     babel: { presets: ['es2015'] }
-  },
-
-  npm: {
-    styles: {
-      'normalize.css': ['normalize.css'],
-      milligram: ['dist/milligram.min.css'],
-      katex: ['dist/katex.min.css']
-    },
-    globals: {
-      Katex: 'katex',
-      Promise: 'bluebird'
-    }
   }
 };
